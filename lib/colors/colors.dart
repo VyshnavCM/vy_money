12faf8facdeff3ad2483ad75c2511db754e0aa49
Vyshnav_Vijayan <vvijayan854@gmail.com>
2023-10-20 14:10:04 +0530

    Color theme changed

--- lib/colors/colors.dart
+++ lib/colors/colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = const Color.fromARGB(255, 14, 135, 125);
+Color primaryColor = Color.fromARGB(255, 49, 178, 218);
 Color backgroundCardCOlor = const Color.fromARGB(255, 14, 23, 16);
 Color backgroundColor = const Color.fromARGB(255, 255, 255, 255);
 Color expenseColor =const Color.fromARGB(255, 234, 50, 36) ;
 Color incomeColor = const Color.fromARGB(255, 13, 161, 36);
-Color mainBackgroundColor =const Color.fromARGB(255, 183, 245, 229);
+Color mainBackgroundColor =Color.fromARGB(255, 183, 223, 245);
